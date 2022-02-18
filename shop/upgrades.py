e2069ac00ad31adc0a119f7f56ef7e9b11217953
ShamenPY <karolswwacha17@gmail.com>
2022-02-18 19:29:23 +0100

    endcommit

---
+++ shop/upgrades.py
@@ -0,0 +1,55 @@
+import pygame
+import pygame as pg
+import os
+import time
+
+class upgrades():
+    def upgrade(self, cost_upgrade, cookie_per_second,quantity):
+        self.cost_upgrade = cost_upgrade
+        self.cookie_per_second = cookies_per_second
+        self.quantity = quantity
+
+x = 0
+
+
+Cursor = upgrades()
+Cursor.cost_upgrade = 25
+Cursor.quantity = 0
+
+Autoclick = upgrades()
+Autoclick.cost_upgrade = 30
+Autoclick.cookies_per_second = 2
+Autoclick.quantity = 0
+
+
+Grandma = upgrades()
+Grandma.cost_upgrade = 200
+Grandma.cookies_per_second = 7
+Grandma.quantity = 0
+
+
+Cookie_farm = upgrades()
+Cookie_farm.cost_upgrade = 5000
+Cookie_farm.cookies_per_second = 50
+Cookie_farm.quantity = 0
+
+
+Cookie_factory = upgrades()
+Cookie_factory.cost_upgrade = 25000
+Cookie_factory.cookies_per_second = 300
+Cookie_factory.quantity = 0
+
+
+Cookie_planet = upgrades()
+Cookie_planet.cost_upgrade = 125000
+Cookie_planet.cookies_per_second = 1000
+Cookie_planet.quantity = 0
+
+
+Cookie_galaxy = upgrades()
+Cookie_galaxy.cost_upgrade = 900000
+Cookie_galaxy.cookies_per_second = 10000
+Cookie_galaxy.quantity = 0
+
+
+
